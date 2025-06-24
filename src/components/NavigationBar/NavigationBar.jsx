ac47d52eb1f219b9eca983e66e5c2bca0041e29f
Cyril Chan <kwunming0911@gmail.com>
2025-06-24 22:51:58 +0100

    Replacing original nav bar with NavigationBar.jsx component

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,4 +1,22 @@
-export default function NavigationBar() {
+import {useState} from "react";
+import styles from './NavigationBar.module.css'
 
-    return <div>NavBar</div>
+export default function NavigationBar({movies}) {
+    const [query, setQuery] = useState("");
+    return <nav className={styles.navBar}>
+        <div className="logo">
+            <span role="img">🍿</span>
+            <h1>usePopcorn</h1>
+        </div>
+        <input
+            className="search"
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+        />
+        <p className={styles.numResults}>
+            Found <strong>{movies.length}</strong> results
+        </p>
+    </nav>
 }
